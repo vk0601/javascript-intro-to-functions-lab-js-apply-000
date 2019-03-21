a6f0622b43cc42a388d13a5e4b2ecb5cf5ea2ee5
Vladimir Gourov <vladimir.gourov@blackrock.com>
2019-03-21 01:50:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return "I can't hear you!"
   }
-  else (string === "I love you, Grandma."){
+  else{ (string === "I love you, Grandma.")
     return "I love you, too."
-  } else {
-    return "YES INDEED!"
   }
 }
