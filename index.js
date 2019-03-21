12d5414de4e51237ad4e322395746f7a12d905ad
Vladimir Gourov <vladimir.gourov@blackrock.com>
2019-03-21 01:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function 
+function sayHiToGrandma(string){
+  if (sting === string.toLowerCase){
+    return "I can't hear you!"
+  }
+}
